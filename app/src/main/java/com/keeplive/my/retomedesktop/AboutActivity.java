de8d00d434f250883d340168ad254e5b4dd706fe
灯塔路 <806215732@qq.com>
2020-06-21 23:48:42 +0800

    自定义验证码

--- app/src/main/java/com/keeplive/my/retomedesktop/AboutActivity.java
+++ app/src/main/java/com/keeplive/my/retomedesktop/AboutActivity.java
@@ -2,7 +2,6 @@ package com.keeplive.my.retomedesktop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.keeplive.my.retomedesktop.wiget.VerifyCodeView;
 
